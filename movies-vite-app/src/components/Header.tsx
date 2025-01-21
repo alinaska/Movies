0b306b856c0fc51750740a4a5d60171607a637d1
flup <flup1703@gmail.com>
2025-01-21 18:48:01 +0300

    refactor: изменил header стал лучше выглядеть и исправил парочку ошибок.

--- movies-vite-app/src/components/Header.tsx
+++ movies-vite-app/src/components/Header.tsx
@@ -4,76 +4,73 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import IconButton from '@mui/material/IconButton';
 import RegistrationForm from './forms/RegistrationForm';
 import LoginForm from './forms/LoginForm';
-import { Button } from '@mui/material';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 import { useState } from 'react';
 
 
 const Header = () => {
-    
-    const [isRegistrationFormOpen, setIsRegistrationFormOpen] = useState(false);
-    const [isLoginFormOpen, setIsLoginFormOpen] = useState(false);
-  
-    const handleOpenRegistrationForm = () => {
-      setIsRegistrationFormOpen(true);
-    };
-  
-    const handleCloseRegistrationForm = () => {
-      setIsRegistrationFormOpen(false);
-    };
-  
-    const handleOpenLoginForm = () => {      
-      setIsLoginFormOpen(true);      
-      
-    };
 
-    const handleCloseLoginForm = () => {
-        setIsLoginFormOpen(false);
-          };      
-  
-    return (
-      <Box
-        component="div"
-        sx={{
-            position:'absolute',
-          top: 0,
-          width: '100%',
-          height: '72px',
-          borderBottom: '1px solid grey',
-          backgroundColor: 'blue',
-          display: 'flex',
-          gap: '1100px',
-          marginBottom: '24px',
-        }}
-      >
-        <Typography
-          variant="h6"
-          sx={{ textAlign: 'left', padding: '18px', color: 'white' }}
-        >
+  const [isRegistrationFormOpen, setIsRegistrationFormOpen] = useState(false);
+  const [isLoginFormOpen, setIsLoginFormOpen] = useState(false);
+  const [isLogined, setIsLogined] = useState(false);
+
+  const handleOpenRegistrationForm = () => {
+    setIsRegistrationFormOpen(true);
+  };
+
+  const handleCloseRegistrationForm = () => {
+    setIsRegistrationFormOpen(false);
+  };
+
+  const handleOpenLoginForm = () => {
+    setIsLoginFormOpen(true);
+
+  };
+
+  const handleCloseLoginForm = () => {
+    setIsLogined(true);
+    setIsLoginFormOpen(false);
+  };
+
+  return (
+    <AppBar>
+      <Toolbar>
+        <Typography variant="h6" sx={{ textAlign: 'left', padding: '18px', color: 'white', flexGrow: 1, }}>
           Фильмы
         </Typography>
+        <Stack>
+          {isLogined ? (
+            <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '72px' }}>
+              <IconButton size="medium" sx={{ color: 'white', width: '32px', height: '32px' }} >
+                <AccountCircleIcon sx={{ width: '32px', height: '32px' }}/>
+              </IconButton>
+            </Box>
+          ) : (
+            <Box>
+              {
+                ['Зарегестрироваться', 'Войти'].map((text, index) => (
+                  <Button key={index} onClick={index === 0 ? handleOpenRegistrationForm : handleOpenLoginForm} sx={{ color: 'white' }}>{text}</Button>
+                ))
+              }
+            </Box>
+          )}
+          <RegistrationForm
+            open={isRegistrationFormOpen}
+            handleClose={handleCloseRegistrationForm}
+            onRequestToken={handleOpenLoginForm}
+          />
+          <LoginForm
+            open={isLoginFormOpen}
+            handleClose={handleCloseLoginForm}
+          />
+        </Stack>
+      </Toolbar>
+    </AppBar>
+  );
+}
 
-       <Box sx={{ display: 'flex', flexDirection: 'row' }}> {['Зарегестрироваться', 'Войти'].map((text, index) => (
-    <Button key={index} onClick={index === 0 ? handleOpenRegistrationForm : handleOpenLoginForm} sx={{ color: 'white' }}>{text}</Button>
-  ))}
-       <IconButton size="medium" sx={{ color: 'white' }} >
 
-          <AccountCircleIcon />
-        </IconButton>
-       </Box>
-
-        <RegistrationForm
-          open={isRegistrationFormOpen}
-          handleClose={handleCloseRegistrationForm}
-          onRequestToken={handleOpenLoginForm}
-        />
-        <LoginForm
-        open={isLoginFormOpen}
-        handleClose={handleCloseLoginForm}
-         
-      />    
-      </Box>
-    );
-  }
-
-
-  export default Header;
+export default Header;
